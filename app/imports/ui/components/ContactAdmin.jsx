d22f7e9450455b340a9e2f6c07c4e842385e3e55
Jay Ramos <89936073+ramosJay@users.noreply.github.com>
2021-11-03 22:55:08 -1000

    contacts collection finished in 35~ minutes

---
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { Card, CardContent, Image } from 'semantic-ui-react';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class ContactAdmin extends React.Component {
+  render() {
+    return (
+      <Card centered>
+        <Card.Content>
+          <Image
+            floated='right'
+            size='mini'
+            src={this.props.contact.image}
+          />
+          <Card.Header>{this.props.contact.firstName} {this.props.contact.lastName}</Card.Header>
+          <Card.Meta>{this.props.contact.address}</Card.Meta>
+          <Card.Description>
+            {this.props.contact.description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+        </Card.Content>
+        <CardContent extra>
+          {this.props.contact.owner}
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+ContactAdmin.propTypes = {
+  stuff: PropTypes.shape({
+    name: PropTypes.string,
+    quantity: PropTypes.number,
+    condition: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+  contact: PropTypes.object.isRequired,
+};
+
+// Wrap this component in withRouter since we use the <Link> React Router element.
+export default withRouter(ContactAdmin);
--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Container, Table, Header, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Stuffs } from '../../api/stuff/Stuff';
-import Contact from '../components/Contact';
-import { Contacts } from '../../api/contact/Contacts';
+import StuffItemAdmin from '../components/StuffItemAdmin';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-class ListContacts extends React.Component {
+class ListStuffAdmin extends React.Component {
 
   // If the subscription(s) have been received, render the page, otherwise show a loading icon.
   render() {
@@ -19,17 +18,27 @@ class ListContacts extends React.Component {
   renderPage() {
     return (
       <Container>
-        <Header as="h2" textAlign="center">List Contacts</Header>
-        <Card.Group>
-          {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact} />)}
-        </Card.Group>
+        <Header as="h2" textAlign="center">List Stuff (Admin)</Header>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Quantity</Table.HeaderCell>
+              <Table.HeaderCell>Condition</Table.HeaderCell>
+              <Table.HeaderCell>Owner</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.props.stuffs.map((stuff) => <StuffItemAdmin key={stuff._id} stuff={stuff} />)}
+          </Table.Body>
+        </Table>
       </Container>
     );
   }
 }
 
 // Require an array of Stuff documents in the props.
-ListContacts.propTypes = {
+ListStuffAdmin.propTypes = {
   stuffs: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
@@ -37,13 +46,13 @@ ListContacts.propTypes = {
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Contacts.userPublicationName);
+  const subscription = Meteor.subscribe(Stuffs.adminPublicationName);
   // Determine if the subscription is ready
   const ready = subscription.ready();
   // Get the Stuff documents
-  const contacts = Contacts.collection.find({}).fetch();
+  const stuffs = Stuffs.collection.find({}).fetch();
   return {
-    contacts,
+    stuffs,
     ready,
   };
-})(ListContacts);
+})(ListStuffAdmin);
